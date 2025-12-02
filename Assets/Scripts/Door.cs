9e36b71d49fa877af1252780a96835d16bc0cb40
Щукин Владимир <RusiSLin@gmail.com>
2025-12-02 20:41:15 +0300

    changed code to work for first rooms

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Door : MonoBehaviour, IInteractable
 {
+    public float roty = 0f;
     public float speed = 90f;
     private bool isOpen = false;
     private float currentAngle = 0f;
-
+    void Start()
+    {
+        roty = transform.localRotation.eulerAngles.y;
+    }
     void Update()
     {
         float targetAngle = isOpen ? 90f : 0f;
         currentAngle = Mathf.MoveTowards(currentAngle, targetAngle, speed * Time.deltaTime);
-        transform.localRotation = Quaternion.Euler(0, 0, currentAngle);
+        transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, roty + currentAngle, transform.localRotation.eulerAngles.z);
     }
 
     public void Interact()
