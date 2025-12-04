1fdf3f9d5e59ca203a7c9393a3f811e9092af9ac
brainfeveralltime <nubasick456@gmail.com>
2025-12-04 17:10:52 +0300

    Create working skull

--- Assets/Scripts/SkullPuzzle.cs
+++ Assets/Scripts/SkullPuzzle.cs
@@ -27,7 +27,8 @@ public class SkullPuzzle : MonoBehaviour, IInteractable
         if (GemInventory.gemInventory.HasGem(requiredColor))
         {
             GemInventory.gemInventory.RemoveGem(requiredColor);
-            Instantiate(gemPrefab, gemSlot);
+            Instantiate(gemPrefab, gemSlot.position, gemSlot.rotation);
+            Debug.Log("Вставили камень");
             SolvePuzzle();
         }
         else
@@ -41,30 +42,31 @@ public class SkullPuzzle : MonoBehaviour, IInteractable
         isSolved = true;
         digitDisplay.text = solutionDigit.ToString();
         StartCoroutine(AnimateDigit());
+        Debug.Log("Запускаем анимацию");
     }
 
     private IEnumerator AnimateDigit()
     {
         float duration = 2f;
         float timer = 0;
-
+        Debug.Log("Запуск анимации появления");
         while (timer < duration)
         {
             digitDisplay.alpha = Mathf.Lerp(0, 1, timer / duration);
             timer += Time.deltaTime;
             yield return null;
         }
-
+        Debug.Log("Окончание");
         timer = 0;
         yield return new WaitForSeconds(1f);
-
+        Debug.Log("Прошла секунда, исчезновение");
         while (timer < duration)
         {
             digitDisplay.alpha = Mathf.Lerp(1, 0, timer / duration);
             timer += Time.deltaTime;
             yield return null;
         }
-
+        Debug.Log("Окончание");
         timer = 0;
         yield return new WaitForSeconds(1f);
     }
