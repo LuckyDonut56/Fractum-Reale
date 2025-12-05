c0e8b667169155f7a60eabe3e1d5401dcc87e17b
Щукин Владимир <RusiSLin@gmail.com>
2025-12-05 04:11:17 +0300

    made door close by itself

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,7 @@ public class Door : MonoBehaviour, IInteractable
 {
     public float roty = 0f;
     public float speed = 90f;
-    private bool isOpen = false;
+    public bool isOpen = false;
     private float currentAngle = 0f;
     void Start()
     {
--- Assets/Scripts/roomchange.cs
+++ Assets/Scripts/roomchange.cs
@@ -1,6 +1,6 @@
 using Unity.VisualScripting;
 using UnityEngine;
-
+using System.Collections;
 public class roomchange : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,20 +12,29 @@ public class roomchange : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
     }
+    IEnumerator OnTriggerEnter()
+    {
+        GameObject.FindGameObjectWithTag("doorroom1").GetComponent<Door>().speed = 360;
+        GameObject.FindGameObjectWithTag("doorroom1").GetComponent<Door>().isOpen = false;
+        yield return new WaitForSeconds(0.1f);
+        foreach (var x in GameObject.FindGameObjectsWithTag("room 1"))
+        {
+            x.SetActive(false);
+        }
+        GameObject.FindGameObjectWithTag("doorroom1").SetActive(false);
+        foreach (var x in GameObject.FindGameObjectsWithTag("room 2"))
+        {
+            x.GetComponent<Transform>().localPosition = Vector3.zero;
+        }
+        gameObject.SetActive(false);
+    }
+
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            foreach(var x in GameObject.FindGameObjectsWithTag("room 1"))
-            {
-                x.SetActive(false);
-            }
-            foreach (var x in GameObject.FindGameObjectsWithTag("room 2"))
-            {
-                x.GetComponent<Transform>().localPosition = Vector3.zero;
-            }
+            StartCoroutine("OnTriggerEnter");
         }
     }
 }
