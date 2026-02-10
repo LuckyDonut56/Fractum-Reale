d6a2d880b5cfaf43059f75e4bfccb438e5f68042
Щукин Владимир <RusiSLin@gmail.com>
2026-02-10 20:33:31 +0300

    made flashlights switchable. added prototype for UV flashlight

--- Assets/Scripts/Fonarik.cs
+++ Assets/Scripts/Fonarik.cs
@@ -1,30 +1,50 @@
 using UnityEngine;
-
+public enum light { none, std, UV };
 public class FlashlightSimple : MonoBehaviour
 {
     public GameObject flashlight;
-    public Light flashlightLight;  
+    public GameObject UVflashlight;
+    public light state = global::light.none;
     public KeyCode toggleKey = KeyCode.F;  
-
-    private bool isOn = false;
     void Start()
     {
         flashlight.SetActive(false);
-        flashlightLight.enabled = false;
+        UVflashlight.SetActive(false);
     }
     void Update()
     {
-        if (Inventory.Instance.hasFlashlight)
+        switch (state)
         {
-            if (!flashlight.activeSelf)
-            {
-                flashlight.SetActive(true);
-            }
-            if (Input.GetKeyDown(toggleKey))
-            {
-                isOn = !isOn;
-                flashlightLight.enabled = isOn;
-            }
+            case global::light.none:
+                flashlight.SetActive(false);
+                UVflashlight.SetActive(false);
+                break;
+            case global::light.std:
+                UVflashlight.SetActive(false);
+                if (Inventory.Instance.hasFlashlight)
+                {
+                    if (!flashlight.activeSelf)
+                    {
+                        flashlight.SetActive(true);
+                    }
+                }
+                break;
+            case global::light.UV:
+                flashlight.SetActive(false);
+                if (Inventory.Instance.hasUVFlashlight)
+                {
+                    if (!UVflashlight.activeSelf)
+                    {
+                        UVflashlight.SetActive(true);
+                    }
+                }
+                break;
+        }
+        if (Input.GetKeyDown(toggleKey))
+        {
+            if (state != global::light.UV)
+            state+=1;
+            else state=0;
         }
 
     }
