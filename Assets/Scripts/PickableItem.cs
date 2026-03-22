2fd125b88750d0ac9890900305368074d4421644
K0STET <kostakozuharev@gmail.com>
2026-03-22 20:22:50 +0300

    Добавлены звуки при подборе предметов

--- Assets/Scripts/PickableItem.cs
+++ Assets/Scripts/PickableItem.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class PickableItem : MonoBehaviour, IInteractable
 {
+    public AudioSource pickUpSound;
     public void Interact()
     {
+        pickUpSound.Play();
         Inventory.Instance.AddObject(gameObject);
     }
 }
