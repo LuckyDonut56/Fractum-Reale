ca54c288a77d8eed9d162cbe1bcd5e17290d7e25
Blackspace1 <125980481+Blackspace1@users.noreply.github.com>
2026-03-23 17:53:25 +0300

    меню паузы

---
+++ Assets/Scripts/Pause menu.cs
@@ -0,0 +1,36 @@
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pausemenu : MonoBehaviour
+{
+    public GameObject PausePanel;
+    public PlayerController PlayerController;
+    public void ContinueButton()
+    {
+        PausePanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1;
+        PlayerController.enabled = true;
+    }
+    public void ExitButton()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause();
+        }
+    }
+    public void pause()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        PlayerController.enabled = false;
+    }
+}
