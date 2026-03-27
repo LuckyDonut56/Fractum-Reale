67b63acdf4b9a642f7217b58999bce004afc59f4
K0STET <kostakozuharev@gmail.com>
2026-03-27 14:19:00 +0300

    Добавлено открытие двери при решении головоломки

---
+++ Assets/Scripts/OpenPictureDoor.cs
@@ -0,0 +1,34 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class OpenPictureDoor : MonoBehaviour, IInteractable
+{
+    public Transform smallColumn;
+    public Transform mediumColumn;
+    public Transform bigColumn;
+    public Transform picture1;
+    public Transform picture2;
+    public Transform picture3;
+    public float rot1;
+    public float rot2;
+    public float rot3;
+  
+    bool IsSolvePuzzle()
+    {
+        if (picture1.IsChildOf(smallColumn) && picture2.IsChildOf(mediumColumn) && picture3.IsChildOf(bigColumn))
+            if (Mathf.Round(picture1.localEulerAngles.x) == rot1 && Mathf.Round(picture2.localEulerAngles.x) == rot2 && Mathf.Round(picture3.localEulerAngles.x) == rot3)
+                return true;
+        return false;
+    }
+    public void Interact()
+    {
+        var door = GetComponent<Door>();
+        if (!door.enabled)
+            door.doorOpen.Play();
+        if(IsSolvePuzzle())
+        {
+            door.enabled = true;
+            enabled = false;
+        }
+    }
+}
